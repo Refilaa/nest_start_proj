b21e498292adac8717f38b150cb3d34991bd4a25
Fulgence <223755639+Refilaa@users.noreply.github.com>
2026-08-24 16:54:42 +0300

    ajout service et methodes in controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
+import { CatsControllerController } from './cats-controller/cats-controller.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CatsControllerController, CatsController],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+
+    constructor(private CatsService: CatsService) {}
+
+    @Post()
+    create(@Body() createCatDto: CreateCatDto) {
+        //return 'This action adds a new cat';
+        this.catsService.create(createCatDto);
+    }
+
+    /* @Get()
+    findAll(@Query() query: ListAllEntities) {
+        return `This action returns all cats (limit: ${query.limit} items)`;
+    } */
+    
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} cat`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+        return `This action updates a #${id} cat`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} cat`;
+    }
+}
